489c042951f5e5743a413520ad386c2902bf70c0
Pham Ngoc Diep <dieppn@sskpi.com>
2020-03-09 12:08:48 +0700

    Thêm middleware compress response và cors

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const cors = require('cors');
+const compression = require('compression');
+
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(compression());
 
 
 // const exampleMiddleware = require('./app/Middleware/ExampleMiddleware');
 // app.use('/',exampleMiddleware({}));
 
-var requestTime = function (req, res, next) {
-    console.log(new Date().getTime())
-    next()
-}
+const requestTime = function (req, res, next) {
+    console.log(new Date().getTime());
+    next();
+    console.log(2, new Date().getTime())
+};
 
-app.use(requestTime)
+app.use(requestTime);
+
 
 const defaultRouter = require('./routers/default');
 app.use('/', defaultRouter);
